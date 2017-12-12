9fe5938844f04220fbc2aadf92ac3bbeafc4f0a2
Corentin Le Molgat <corentinl@google.com>
2017-12-12 17:17:40 +0100

    java fixup

--- cmake/java.cmake
+++ cmake/java.cmake
@@ -2,7 +2,10 @@ if (NOT BUILD_JAVA)
 	return()
 endif()
 
-find_package(SWIG)
-find_package(JAVA)
-find_package(JNI)
+find_package(SWIG REQUIRED)
+find_package(JAVA REQUIRED)
+find_package(JNI REQUIRED)
 
+if(NOT TARGET ortools::ortools)
+	message(FATAL_ERROR "Java: missing ortools TARGET")
+endif()
