0aa4e6132990eb3295b77e9b334469d047b86222
mizux <corentinl@google.com>
2017-12-22 15:49:07 +0100

    fix swig on windows

--- cmake/external/swig.cmake
+++ cmake/external/swig.cmake
@@ -1,27 +1,16 @@
-set(SWIG_URL http://prdownloads.sourceforge.net/swig/swigwin-3.0.12.zip)
+# Download and unpack swig at configure time
+configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.swig swig-download/CMakeLists.txt)
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/swig-download )
+if(result)
+  message(FATAL_ERROR "CMake step for swig failed: ${result}")
+endif()
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/swig-download )
+if(result)
+  message(FATAL_ERROR "Build step for swig failed: ${result}")
+endif()
 
-set_property(DIRECTORY PROPERTY EP_BASE dependencies)
-
-ExternalProject_Add(SWIG_project
-	URL ${SWIG_URL}
-	BUILD_IN_SOURCE 1
-	UPDATE_COMMAND ""
-	CONFIGURE_COMMAND ""
-	BUILD_COMMAND ""
-	INSTALL_COMMAND ""
-	TEST_COMMAND ""
-	LOG_DOWNLOAD ON
-	LOG_CONFIGURE ON
-	LOG_BUILD ON
-	)
-
-# Specify include dir
-ExternalProject_Get_Property(SWIG_project source_dir)
-ExternalProject_Get_Property(SWIG_project binary_dir)
-
-# Executable
-add_executable(SWIG_EXECUTABLE STATIC IMPORTED)
-set_target_properties(SWIG_EXECUTABLE PROPERTIES IMPORTED_LOCATION
-	${binary_dir}/swigwin-3.0.12/swig.exe)
-add_dependencies(SWIG_EXECUTABLE SWIG_project)
-
+set(SWIG_EXECUTABLE ${CMAKE_BINARY_DIR}/swig/swig.exe)
